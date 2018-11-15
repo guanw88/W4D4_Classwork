6a545c09eaeb2d4c4db1de4a6d32d8c65fbd8054
gwang <guanw88@gmail.com>
2018-11-15 13:27:14 -0800

    Create Band model, migration, validations, controller, and views

---
+++ music_app/app/controllers/bands_controller.rb
@@ -0,0 +1,53 @@
+class BandsController < ApplicationController
+
+  def index #show all bands
+    render :index
+  end
+
+  def create #create new band
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to band_url(@band)
+    else
+      flash[:errors] = @band.errors.full_messages
+      render :new
+    end
+  end
+
+  def new #show create new band page
+    render :new
+  end
+
+  def edit #show edit band page
+    @band = Band.find_by(id: params[:id])
+    render :edit
+  end
+
+  def show #show individual band page
+    @band = Band.find_by(id: params[:id])
+    render :show
+  end
+
+  def update #update existing band
+    @band = Band.find_by(id: params[:id])
+    @band.update(band_params)
+    if @band.save
+      redirect_to band_url(@band)
+    else
+      flash[:errors] = @band.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy #delete band
+    @band = Band.find_by(id: params[:id])
+    @band.destroy if @band
+    redirect_to bands_url
+  end
+
+  private
+  def band_params
+    params.require(:band).permit(:name)
+  end
+
+end
